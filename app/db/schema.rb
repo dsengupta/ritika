57934788a9b5e7bc7d70e68824b56a1ba585369b
ritzi33 <ritikaagarwal.it33@gmail.com>
2012-07-15 01:02:07 -0400

    final commit of the assignment on Rails

--- app/db/schema.rb
+++ app/db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(:version => 20120714034431) do
 
   create_table "users", :force => true do |t|
     t.string   "name",       :null => false
-    t.string   "email",      :null => false
+    t.string   "email"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
