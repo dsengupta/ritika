30167e60e16ddad3fe636ace1683f7acdd438b21
ritzi33 <ritikaagarwal.it33@gmail.com>
2012-08-04 22:53:31 -0400

    removed unwanted files

--- app/app/controllers/users_controller.rb
+++ app/app/controllers/users_controller.rb
@@ -8,21 +8,23 @@ def show
 end
 def new
 @user=User.new
-
 end
+
 def create 
 @user=User.create!(params[:user])
 UserMailer.welcome_email(@user).deliver
 redirect_to users_path
-
 end
+
 def destroy
 User.find(params[:id]).destroy
 redirect_to users_path
 end 
+
 def edit 
 @user= User.find(params[:id])
 end
+
 def update
 @user = User.find(params[:id])
 @user.update_attributes(params[:user])
