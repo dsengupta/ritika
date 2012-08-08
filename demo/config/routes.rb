c7e6531ca5cca2b2589f32e590797df3f8a79fe6
ritzi33 <ritikaagarwal.it33@gmail.com>
2012-08-07 21:41:46 -0400

    layout and structuring done

--- demo/config/routes.rb
+++ demo/config/routes.rb
@@ -1,10 +1,11 @@
 Demo::Application.routes.draw do
   get "static_pages/home"
-
   get "static_pages/help"
 
+ 
    resources:users
-   resources:microposts 
+   resources:microposts
+ 
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -54,7 +55,7 @@ Demo::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'users#index'
+   root :to => 'static_pages#home'
 
   # See how all your routes lay out with "rake routes"
 
