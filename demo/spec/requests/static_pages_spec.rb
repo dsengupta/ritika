b90b1e62eef0d72050081d51a5cdc3b515f37719
ritzi33 <ritikaagarwal.it33@gmail.com>
2012-08-24 02:34:24 -0400

    testing started

--- demo/spec/requests/static_pages_spec.rb
+++ demo/spec/requests/static_pages_spec.rb
@@ -1,15 +1,68 @@
 require 'spec_helper'
 
-describe "StaticPages" do
-  describe "home page" do
-     it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      page.should have_content('Sample App')
-    end
-it "should have the title 'Home'" do
-      visit '/static_pages/home'
-      page.should have_selector('title',
-                        :text => "Ritika | Home")
+describe "Static pages" do
+
+  subject { page }
+
+  describe "Home page" do
+    before { visit root_path }
+
+    it { should have_selector('h1',    text: 'Sample App') }
+    it { should have_selector('title', text: full_title('')) }
+
+    describe "for signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
+        FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
+        sign_in user
+        visit root_path
+      end
+
+      it "should render the user's feed" do
+        user.feed.each do |item|
+          page.should have_selector("li##{item.id}", text: item.content)
+        end
+      end
+
+      describe "follower/following counts" do
+        let(:other_user) { FactoryGirl.create(:user) }
+        before do
+          other_user.follow!(user)
+          visit root_path
+        end
+
+        it { should have_link("0 following", href: following_user_path(user)) }
+        it { should have_link("1 followers", href: followers_user_path(user)) }
+      end
+
     end
   end
+
+  describe "Help page" do
+    before { visit help_path }
+
+   
+    it "should have the h1 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('h1', :text => 'Help')
+      page.should have_selector('title', text: full_title('Help'))
+    end
+  end
+
+  describe "About page" do
+    it "should have the h1 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector('h1', :text => 'About Us')
+      page.should have_selector('title', text: full_title('About Us'))
+    end
+  end
+
+describe "Contact page" do
+  it "should have the h1 'About Us'" do
+    visit '/static_pages/contact'
+    page { should have_selector('h1',    text: 'Contact') }
+    page { should have_selector('title', text: full_title('Contact')) }
+  end
+end
 end
