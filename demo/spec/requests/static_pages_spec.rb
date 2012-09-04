1a5bb3af72612827b556f5e393fd774fddfa5ce4
ritika <ritikaagarwal.it33@gmail.com>
2012-09-04 16:52:32 +0530

    testing continued

--- demo/spec/requests/static_pages_spec.rb
+++ demo/spec/requests/static_pages_spec.rb
@@ -9,6 +9,7 @@ describe "Static pages" do
 
     it { should have_selector('h1',    text: 'Sample Application') }
     it { should have_selector('title', text: full_title('')) }
+    it { should_not have_selector 'title', text: '| Home' }
 
     describe "for signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
